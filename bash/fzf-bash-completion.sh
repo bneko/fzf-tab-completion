28082f117507f4595910c1e4d04493e2212c8931
bneko <52762964+bneko@users.noreply.github.com>
2025-07-07 13:54:07 +0000

    bash: add FZF_COMPLETION_START_TIME_UTC_MS

--- bash/fzf-bash-completion.sh
+++ bash/fzf-bash-completion.sh
@@ -282,6 +282,7 @@ fzf_bash_completion() {
     local COMPREPLY=
     FZF_COMPLETION_PID=$$ \
     FZF_COMPLETION_CUR=${raw_cur} \
+    FZF_COMPLETION_START_TIME_UTC_MS=$(date -u +%s%3N) \
     fzf_bash_completer "$cmd" "$cur" "$prev"
     if [ -n "$COMPREPLY" ]; then
         if [ -n "$raw_cur" ]; then
